74e1a0bfac7e2058b9f2320d60623950d59c433e
whoiscris <ga57far@mytum.de>
2015-03-11 01:10:17 +0100

    init solarized git repos, tmux, xfce solarize theme

--- .init.sh
+++ .init.sh
@@ -12,13 +12,26 @@ fi
 
 sudo /bin/bash .apt-get.sh
 
+# git clones
+/bin/bash .git-clones.sh
+
 # get git submodules for this repository too.
 echo "getting git submodules ..."
 git submodule init
 git submodule update
 
+
+# create directories
+mkdir --parents ~/.dir_colors
+cd ~/.dir_colors
+ln --symbolic --force ~/dircolors-solarized/dircolors.ansi-light .
+cd -
+grep --quiet '$TERM != "screen" ]] && exec tmux' ~/.bashrc || echo '[[ $TERM != "screen" ]] && exec tmux' >> ~/.bashrc
+grep --quiet 'eval `dircolors ~/.dir_colors/dircolors.ansi-light`' ~/.bashrc || echo 'eval `dircolors ~/.dir_colors/dircolors.ansi-light`' >> ~/.bashrc
+
+# cp 
+cp .tmux.conf ~/.tmux.conf
 # make symlinks to /etc/.vimrc and /etc/vim
-echo "creating symlinks to vim and .vimrc ..."
 /bin/bash .makes-symlinks.sh
 
 
